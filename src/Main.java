b83a6f27e5aef4fed71603374e5bfef0243be2fd
aronkaczur <kishanczur@gmail.com>
2020-06-10 11:51:21 +0200

    Listing added

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(Arrays.toString(args));
 
         if (args[0].equals(" ")){
             nothing();
+        }
+        else if(args[0].equals("-l")){
+            listing();
         }
     }
 
@@ -18,5 +26,13 @@ public class Main {
         System.out.println("    -a   Adds a new task");
         System.out.println("    -r   Removes an task");
         System.out.println("    -c   Completes an task");
+    }
+
+    public static void listing() throws IOException {
+        Path filePath = Paths.get("assets/tasks.txt");
+        List<String> lines = Files.readAllLines(filePath);
+        for (int i = 0; i < lines.size(); i++) {
+            System.out.println(i+1 +" " +lines.get(i));
+        }
     }
 }
